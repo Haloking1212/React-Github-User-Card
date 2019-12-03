33471d19492090676075e9554a7369c4cbc965af
Hector Valdez <hvaldez180@gmail.com>
2019-12-03 18:35:48 -0500

    completed steps one and steps two

--- react-github/src/App.js
+++ react-github/src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-class App extends React.Component() {
-  constructor() {
-    super();
-    this.state = {
-      intialState: ""
-      }
-    }
+class App extends React.Component {
+  state = {
+    intialState: null,
+    name: null,
+    picture: null
+  };
+
+  componentDidMount() {
+    axios.get("https://api.github.com/users/haloking1212").then(res => {
+      console.log(res.data, "haloking1212");
+      this.setState({ picture: res.data.avatar_url,
+      name: res.data.login });
+    });
+  }
 
   render() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1>Welcome to my github followers</h1>
+        <p>{this.state.name}</p>
+        <img src={this.state.picture} />
+      </div>
+    );
   }
 }
 
