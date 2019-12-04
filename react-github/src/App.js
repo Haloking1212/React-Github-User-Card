b300b9514f2daa9893a1d0a96af96adb022cfe20
Hector Valdez <hvaldez180@gmail.com>
2019-12-03 21:34:03 -0500

    completed steps 3 and 4 and also styled

--- react-github/src/App.js
+++ react-github/src/App.js
@@ -4,28 +4,50 @@ import "./App.css";
 
 class App extends React.Component {
   state = {
-    intialState: null,
-    name: null,
-    picture: null
+    me: null,
+    login: null,
+    img: null,
+    followers: []
   };
 
   componentDidMount() {
-    axios.get("https://api.github.com/users/haloking1212").then(res => {
-      console.log(res.data, "haloking1212");
-      this.setState({ picture: res.data.avatar_url,
-      name: res.data.login });
-    });
+    axios
+      .get(`https://api.github.com/users/haloking1212`)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          me: res.data.avatar_url,
+          login: res.data.login,
+          img: res.data.avatar_url
+        });
+      })
+      .catch(err => console.log(err));
+    axios
+      .get(`https://api.github.com/users/haloking1212/followers`)
+      .then(res => {
+        console.log(res.data, "followers");
+        this.setState({
+          followers: res.data
+        });
+      });
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Welcome to my github followers</h1>
-        <p>{this.state.name}</p>
-        <img src={this.state.picture} />
+        <h1>Friends and Followers</h1>
+        <h3>{this.state.login}</h3>
+        <img src={this.state.img} />
+        {this.state.followers.map(friend => {
+          return (
+            <div key={friend.id}>
+              <h3>{friend.login}</h3>
+              <img src={friend.avatar_url} />
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
-
 export default App;
